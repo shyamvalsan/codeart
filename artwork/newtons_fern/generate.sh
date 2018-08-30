bcbcf80dc0b24903ac7b080b5581dee01281796d
root <root@TPC-D12-17.phaedrus.sandvine.com>
2018-08-30 10:01:18 +0000

    Re-structured and added first generative code art

---
+++ artwork/newtons_fern/generate.sh
@@ -0,0 +1,3 @@
+python ../../fractal_generators/fractal-fern.py
+python ../../fractal_generators/fractal-newton.py
+th ../../neural_mixer/neural_mixer.lua -style_image ../../input/fractals/newtonFr.png  -content_image ../../input/fractals/fern.png -image_size 800 -gpu 0 -print_iter 0 -save_iter 0 -output_image newtons_fern.png
